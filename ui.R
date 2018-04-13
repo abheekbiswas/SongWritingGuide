97dea36f8e5076622fee51d04873acabe0c1072d
abheekbiswas <b.abheek@gmail.com>
2018-04-13 18:57:59 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -6,13 +6,6 @@ library(plotly)
 library(plyr)
 set.seed(300)
 
-# Data Pre-processing ----
-# Full data set can be downloaded from: 
-# https://storage.googleapis.com/kaggle-datasets/636/1222/lyrics.csv.zip
-
-lyrics_train <- read.csv("lyrics_train.csv", header = T, stringsAsFactors = FALSE)
-#lyrics_train <- subset(lyrics_train1, year >= 1968 & year <= 2018)
-
 
 #Define UI ---- 
 ui <- fluidPage(
